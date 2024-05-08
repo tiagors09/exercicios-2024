e1a12a5b7c7dbe3b1e8df613c936b0823982f68d
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-07 22:11:29 -0300

    feat: Adicionar modelo de palestrante

---
+++ dart/lib/models/people.dart
@@ -0,0 +1,19 @@
+class People {
+  final int id;
+  final String title;
+  final String name;
+  final String instituition;
+  final Map<String, String> bio;
+  final String picture;
+  final Map<String, dynamic> role;
+
+  People(
+    this.id,
+    this.title,
+    this.name,
+    this.instituition,
+    this.bio,
+    this.picture,
+    this.role,
+  );
+}
