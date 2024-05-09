1957e4e5ae5b5e3067bd6d2b7c6f08c982d0a9c6
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-08 22:34:08 -0300

    feat: Adicionar provider de atividades e rotas

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:chuva_dart/providers/activity_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const ChuvaDart());
@@ -8,15 +10,33 @@ void main() {
 class ChuvaDart extends StatelessWidget {
   const ChuvaDart({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (ctx, st) => const Scaffold(
+            body: Placeholder(),
+          ),
+        ),
+      ],
+    );
+
+    return ChangeNotifierProvider(
+      create: (ctx) {
+        final activityProvider = ActivityProvider();
+        activityProvider.fetchAllActivities();
+
+        return activityProvider;
+      },
+      child: MaterialApp.router(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routerConfig: router,
       ),
-      home: const Placeholder(),
     );
   }
 }
