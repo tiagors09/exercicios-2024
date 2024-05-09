b874cf9fb64a0352c8f3bf4332e989df73470ff9
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-08 21:11:45 -0300

    feat: Adicionar dio para consultas http e url base de consulta

--- dart/lib/service/activity_service.dart
+++ dart/lib/service/activity_service.dart
@@ -1,1 +1,8 @@
-class ActivityService {}
+import 'package:dio/dio.dart';
+
+import '../utils/environment.dart';
+
+class ActivityService {
+  final _dio = Dio();
+  var _baseUrl = Environment.baseUrl;
+}
