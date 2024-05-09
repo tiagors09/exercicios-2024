f051d2c17cd5252c99831cb8c3410399ace5626f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-08 21:12:12 -0300

    feat: Adicionar função de busca de atividades

--- dart/lib/service/activity_service.dart
+++ dart/lib/service/activity_service.dart
@@ -1,8 +1,25 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
+import '../models/activity.dart';
 import '../utils/environment.dart';
 
 class ActivityService {
   final _dio = Dio();
   var _baseUrl = Environment.baseUrl;
+
+  Future<List<Activity>> fetchActivities() async {
+    try {
+      final response = await _dio.get(_baseUrl);
+
+      final Map<String, dynamic> data = jsonDecode(response.data) ?? {};
+
+      print(data);
+
+      return Future.value(<Activity>[]);
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
