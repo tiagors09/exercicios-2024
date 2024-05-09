4616e723ed664d8f33f5ba4a3b7eff6f3f673150
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-08 22:42:57 -0300

    feat: Adicionar item de lista de atividades

---
+++ dart/lib/widgets/activity_list_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ActivityListItem extends StatelessWidget {
+  const ActivityListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
