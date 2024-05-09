20055fe1e0ac53055917259efe8dd448c7532ccb
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-08 22:50:12 -0300

    feat: Adicionar código de renderização

---
+++ dart/lib/widgets/activities_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ActivitiesList extends StatelessWidget {
+  const ActivitiesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemCount: 0, itemBuilder: (ctx, index) => null);
+  }
+}
