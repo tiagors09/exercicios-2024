cc08db0522c75f00b1d558700f93b603d66c5829
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-08 22:18:09 -0300

    feat: Pegar dados da resposta da api

--- dart/lib/providers/activity_provider.dart
+++ dart/lib/providers/activity_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:chuva_dart/models/activity.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import '../utils/environment.dart';
@@ -7,13 +8,25 @@ import '../utils/environment.dart';
 class ActivityProvider with ChangeNotifier {
   final _baseUrl = Environment.baseUrl;
   final _dio = Dio();
+  late int _totalActivitiesCount;
+  late Map<String, String> _links;
+  late List<Activity> _activities;
 
-  Future<dynamic> fetchAllActivities() async {
+  List<Activity> get activities => [..._activities];
+  int get activitiesCount => _activities.length;
+  int get totalActivitiesCount => _totalActivitiesCount;
+  String get nextLink => _links['next'].toString();
+
+  Future<void> fetchAllActivities() async {
     try {
       final response = await _dio.get(_baseUrl);
       final decodedResponse = jsonDecode(response.data);
 
-      return Future.value(decodedResponse);
+      _totalActivitiesCount = decodedResponse['count'];
+      _links = decodedResponse['links'];
+      for (var element in (decodedResponse['data'] as List<dynamic>)) {
+        _activities.add(Activity.fromJson(element));
+      }
     } catch (e) {
       rethrow;
     }
